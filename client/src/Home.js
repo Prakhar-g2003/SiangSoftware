0d6197b4d710289a3fe4a16bda60a7c8ad054554
tyagisatvik <satviktyagi3@gmail.com>
2024-02-04 01:36:30 +0530

    home

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import Navbar from "./Components/ProfilePage/Navbar";
+import "./Home.css"
+import ProjectSlider from "./Components/ProjectSlider/ProjectSlider";
 
-function Home() {
+function Home({projects}) {
     useEffect(() => {
         const getUserinfo = async() => {
             try{
@@ -25,11 +27,19 @@ function Home() {
         
         <div>
         <Navbar/>
+        <img src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dGVjaG5vbG9neXxlbnwwfHwwfHx8MA%3D%3D" alt="Description of the image" />
+        <div className="text">
+            <h1 className="hone">Random Heading</h1>
+        </div>
+        <div className="para">
+            <p className="pone">lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+        </div>
         
         <p>
             
             {localStorage.getItem("authToken")}
         </p>
+        <ProjectSlider projects={projects}/>
         </div>
     );
     }
