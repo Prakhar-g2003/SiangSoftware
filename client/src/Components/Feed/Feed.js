c57c22824d1176cd37c1a26639b80f3dfc788212
shashwat <shashwatskr2019@gmail.com>
2024-02-05 22:52:29 +0530

    home page

---
+++ client/src/Components/Feed/Feed.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Feed.css'
+import ProjectCard from '../../Pages/Projects/ProjectCard'
+
+function Feed({projects}) {
+  return (
+    <div className='feed'>
+        <div className='feed_starting'>
+            <h1>Hi Satvik!</h1>
+            <div className="projects-section">
+              {
+                projects&&projects.map((item,index)=>(
+                  <ProjectCard key = {index} className={item ? 'specialCard' : ''} project={item}></ProjectCard>
+
+                ))
+              }
+            </div>
+            
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default Feed
---
+++ client/src/Components/TopContributors/TopContributors.js
@@ -0,0 +1,65 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+function createData(name, calories) {
+  return { name, calories }; // Only include necessary columns
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159),
+  createData('Ice cream sandwich', 237),
+  createData('Eclair', 262),
+  createData('Cupcake', 305),
+  createData('Gingerbread', 356),
+];
+
+export default function TopContributors() {
+  return (
+    <TableContainer component={Paper} sx={{ width: '250px' ,height:'100%'}}> 
+      <Table sx={{ minWidth: 100 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>Top Contributors</StyledTableCell>
+            <StyledTableCell align="right"></StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="right">{row.calories}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
