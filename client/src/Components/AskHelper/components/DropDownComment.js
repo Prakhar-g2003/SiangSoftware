1b7ae6219d280c307578f93598a8031667e88ed9
shashwat <shashwatskr2019@gmail.com>
2024-02-08 23:16:22 +0530

    ask helper modified

--- client/src/Components/AskHelper/components/DropDownComment.js
+++ client/src/Components/AskHelper/components/DropDownComment.js
@@ -15,11 +15,21 @@ function CommentDropdown(props) {
       </button>
       <div className="commentdisplay">
         {showComments && (
-          <ul class="askHelperAnswers">
+          <div class="askHelperAnswers">
             {props.comments.map((comment) => (
-              <li class="askHelpBullets">{comment.ans_info}</li>
+              <div className="askHelperCommentanscontainer">
+                <img
+                  class="doubtCardUserPhoto"
+                  src="https://images.unsplash.com/photo-1706887577952-2c3237ba079e?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D"
+                />
+                <div className="askHelperCommentContentcont">
+                  <div className="askHelperCommentuser">Shashwat</div>
+                  <div className="askHelperCommentCont">Lorem ipsum dolor sit amet consectetur adipis</div>
+                </div>
+                
+              </div>
             ))}
-          </ul>
+          </div>
         )}
       </div>
     </div>
