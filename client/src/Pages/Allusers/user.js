cbacfc9e09a4aa21548e841c9f9d455b8f464e34
Sachin <Sachinvarmaiitjee@gmail.com>
2024-02-08 23:47:14 +0530

    sachin changes

---
+++ client/src/Pages/Allusers/user.js
@@ -0,0 +1,50 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Sachin Verma',
+    username: 'Bret',
+    branch:"Mechanical"
+  },
+  {
+    id: 2,
+    name: 'Priyanshi Gupta',
+    username: 'Antonette',
+    branch: 'Civil'
+  },
+  {
+    id: 3,
+    name: 'Manoj Gupta',
+    username: 'Samantha',
+    branch: 'CSE'
+  },
+  {
+    id: 4,
+    name: 'Manisha Gupta',
+    username: 'Karianne',
+    branch: 'CSE'
+  },
+  {
+    id: 5,
+    name: 'Sachin Verma',
+    username: 'Bret',
+    branch:"Mechanical"
+  },
+  {
+    id: 6,
+    name: 'Priyanshi Gupta',
+    username: 'Antonette',
+    branch: 'Civil'
+  },
+  {
+    id: 7,
+    name: 'Manoj Gupta',
+    username: 'Samantha',
+    branch: 'CSE'
+  },
+  {
+    id: 8,
+    name: 'Manisha Gupta',
+    username: 'Karianne',
+    branch: 'CSE'
+  }
+];
